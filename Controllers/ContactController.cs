5037a41cd3dd67831b1935aba80e0dd6e78e0489
Makadown <maseflores79@gmail.com>
2019-05-18 19:34:53 -0500

    metiendo primer version de controlador

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using contactos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace contactos.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly ContactosContext _context;
+
+        public ContactController(ContactosContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Contacto> GetAll()
+        {
+            return _context.Contacto.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contacto>> GetById(long id)
+        {
+            var item = await _context.Contacto.FindAsync(id);
+            if(item==null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Contacto>> Create([FromBody] Contacto item)
+        {
+            if(item==null)
+            {
+                return BadRequest();
+            }
+
+            _context.Contacto.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new {id = item.id}, item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(long id, [FromBody] Contacto item)
+        {
+            if(item == null || id==0)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var contact = await _context.Contacto.FindAsync(id);
+
+            if(contact==null)
+            {
+                return NotFound();
+            }
+
+            _context.Contacto.Remove(contact);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
